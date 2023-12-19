fb443353c8269b9a2eff435d440ced706f687c07
Elias Makhonjwa <126951032+U18034332@users.noreply.github.com>
2023-12-19 06:47:32 +0200

    Added darkmode and still working on code folding

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,17 @@
-var editor = CodeMirror.fromTextArea(document.getElementById("code-editor"), {
+window.editor_python = CodeMirror.fromTextArea(document.getElementById("code-editor"), {
     lineNumbers: true,
     mode: "python",
+    theme: "monokai",
     matchBrackets: true,
     autoCloseBrackets: true,
+    smartIndent: true,
+    lint: true,
+    fullScreen: false,
+    extraKeys: {"Ctrl-Q": function(cm){ cm.foldCode(cm.getCursor()); }},
     foldGutter: true,
     gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-    extraKeys: {
-        "Ctrl-Space": "autocomplete",
-        "Ctrl-Q": function(cm) {
-            cm.foldCode(cm.getCursor());
-        },
-        "Ctrl-F": "findPersistent",
-        "Ctrl-H": "replace"
-    },
-    lint: true,
     styleActiveLine: true,
     placeholder: "Write your Python code here",
 });
+
+
